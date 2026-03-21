69eb2dedc0b3b5387192819297c9bd14e9ce01ae
neclor <neclordev@gmail.com>
2026-03-21 21:24:52 +0100

    add temp

--- src/modules/server_manager.py
+++ src/modules/server_manager.py
@@ -10,12 +10,20 @@ logger: logging.Logger = logging.getLogger(__name__)
 
 def get_status() -> str:
     cpu_percent: float = psutil.cpu_percent(interval=1)
-    memory_percent = psutil.virtual_memory().percent
-    disk_percent = psutil.disk_usage("/").percent
-    uptime = str(timedelta(seconds=int(time.time() - psutil.boot_time())))
+    memory_percent: float = psutil.virtual_memory().percent
+    disk_percent: float = psutil.disk_usage("/").percent
+    uptime: str = str(timedelta(seconds=int(time.time() - psutil.boot_time())))
+
+    temp: float = 0.0
+    try:
+        temps: dict = psutil.sensors_temperatures()
+        cores = temps["coretemp"]
+        temp = sum(t.current for t in cores) / len(cores)
+    except Exception: pass
 
     status_message: str = f"""
 **CPU** - {cpu_percent}%
+**Temp** - {temp}%
 **Memory** - {memory_percent}%
 **Disk** - {disk_percent}%
 **Uptime** - {uptime}
